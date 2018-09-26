ce25dfb67c4dbaf44f1017947d5152f6b4132991
kim yongbin <kybinz@gmail.com>
2018-09-26 20:14:07 +0900

    AddThumbnail 함수를 thumbnail.go 파일로 옮김

--- db.go
+++ db.go
@@ -3,11 +3,8 @@ package roi
 import (
 	"database/sql"
 	"fmt"
-	"image"
 	_ "image/jpeg"
-	"image/png"
 	"log"
-	"os"
 	"sort"
 	"strings"
 
@@ -337,50 +334,4 @@ func FindShot(db *sql.DB, prj string, shot string) (Shot, error) {
 		return Shot{}, err
 	}
 	return s, nil
-}
-
-// 할일: 이 함수는 db와 상관이 없다. 파일 이름을 바꾸거나 다른 파일로 옮기자.
-//
-// AddThumbnail은 특정 샷의 썸네일을 등록한다.
-// 썸네일은 roi안에 파일로 저장된다.
-func AddThumbnail(prj, shot, thumbf string) error {
-	// wrap은 AddThumbnail에서 에러가 났을 때 에러 내용에 기본적인 정보를 추가한다.
-	wrap := func(err error) error {
-		return fmt.Errorf("AddThumbnail: %s", err)
-	}
-
-	fi, err := os.Stat(thumbf)
-	if err != nil {
-		return wrap(err)
-	}
-	maxKB := int64(200)
-	if fi.Size() > (maxKB << 10) {
-		return wrap(fmt.Errorf("file size is bigger than %sKB: %s", maxKB))
-	}
-	from, err := os.Open(thumbf)
-	if err != nil {
-		return wrap(err)
-	}
-	defer from.Close()
-	// thumbf가 지원하는 이미지 파일이 맞는지 확인한다.
-	img, _, err := image.Decode(from)
-	if err != nil {
-		return wrap(err)
-	}
-	// 이미지를 png 이미지로 변경한다.
-	// 파일을 부를때 일일이 파일 확장자를 검사하지 않기 위함이다.
-	if err := os.MkdirAll(fmt.Sprintf("roi-userdata/thumbnail/%s", prj), 0755); err != nil {
-		if !os.IsExist(err) {
-			return wrap(err)
-		}
-	}
-	to, err := os.Create(fmt.Sprintf("roi-userdata/thumbnail/%s/%s.png", prj, shot))
-	if err != nil {
-		return wrap(err)
-	}
-	defer to.Close()
-	if err := png.Encode(to, img); err != nil {
-		return wrap(err)
-	}
-	return nil
 }
---
+++ thumbnail.go
@@ -0,0 +1,52 @@
+package roi
+
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"os"
+)
+
+// AddThumbnail은 특정 샷의 썸네일을 등록한다.
+// 썸네일은 roi안에 파일로 저장된다.
+func AddThumbnail(prj, shot, thumbf string) error {
+	// wrap은 AddThumbnail에서 에러가 났을 때 에러 내용에 기본적인 정보를 추가한다.
+	wrap := func(err error) error {
+		return fmt.Errorf("AddThumbnail: %s", err)
+	}
+
+	fi, err := os.Stat(thumbf)
+	if err != nil {
+		return wrap(err)
+	}
+	maxKB := int64(200)
+	if fi.Size() > (maxKB << 10) {
+		return wrap(fmt.Errorf("file size is bigger than %sKB: %s", maxKB))
+	}
+	from, err := os.Open(thumbf)
+	if err != nil {
+		return wrap(err)
+	}
+	defer from.Close()
+	// thumbf가 지원하는 이미지 파일이 맞는지 확인한다.
+	img, _, err := image.Decode(from)
+	if err != nil {
+		return wrap(err)
+	}
+	// 이미지를 png 이미지로 변경한다.
+	// 파일을 부를때 일일이 파일 확장자를 검사하지 않기 위함이다.
+	if err := os.MkdirAll(fmt.Sprintf("roi-userdata/thumbnail/%s", prj), 0755); err != nil {
+		if !os.IsExist(err) {
+			return wrap(err)
+		}
+	}
+	to, err := os.Create(fmt.Sprintf("roi-userdata/thumbnail/%s/%s.png", prj, shot))
+	if err != nil {
+		return wrap(err)
+	}
+	defer to.Close()
+	if err := png.Encode(to, img); err != nil {
+		return wrap(err)
+	}
+	return nil
+}
