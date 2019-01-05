85c9f0ffa8c2ad423c310585942dc7ae6b3db05e
kim yongbin <kybinz@gmail.com>
2019-01-05 22:01:57 +0900

    Project에서 ordMap 삭제 및 실제 DB를 이용한 테스트로 변경

--- project.go
+++ project.go
@@ -1,7 +1,11 @@
 package roi
 
 import (
+	"database/sql"
+	"errors"
+	"fmt"
 	"regexp"
+	"strings"
 	"time"
 )
 
@@ -35,6 +39,55 @@ type Project struct {
 	ViewLUT    string
 }
 
+func (p *Project) dbValues() []interface{} {
+	if p == nil {
+		p = &Project{}
+	}
+	vals := []interface{}{
+		p.ID,
+		p.Name,
+		p.Status,
+		p.Client,
+		p.Director,
+		p.Producer,
+		p.VFXSupervisor,
+		p.VFXManager,
+		p.CGSupervisor,
+		p.CrankIn,
+		p.CrankUp,
+		p.StartDate,
+		p.ReleaseDate,
+		p.VFXDueDate,
+		p.OutputSize,
+		p.ViewLUT,
+	}
+	return vals
+}
+
+var ProjectTableKeys = []string{
+	"id",
+	"name",
+	"status",
+	"client",
+	"director",
+	"producer",
+	"vfx_supervisor",
+	"vfx_manager",
+	"cg_supervisor",
+	"crank_in",
+	"crank_up",
+	"start_date",
+	"release_date",
+	"vfx_due_date",
+	"output_size",
+	"view_lut",
+}
+
+var ProjectTableIndices = []string{
+	"$1", "$2", "$3", "$4", "$5", "$6", "$7", "$8", "$9", "$10",
+	"$11", "$12", "$13", "$14", "$15", "$16",
+}
+
 var ProjectTableFields = []string{
 	"uniqid UUID PRIMARY KEY DEFAULT gen_random_uuid()",
 	"id STRING NOT NULL UNIQUE CHECK (LENGTH(id) > 0) CHECK (id NOT LIKE '% %')",
@@ -46,33 +99,95 @@ var ProjectTableFields = []string{
 	"vfx_supervisor STRING NOT NULL",
 	"vfx_manager STRING NOT NULL",
 	"cg_supervisor STRING NOT NULL",
-	"crank_in DATE NOT NULL",
-	"crank_up DATE NOT NULL",
-	"start_date DATE NOT NULL",
-	"release_date DATE NOT NULL",
-	"vfx_due_date DATE NOT NULL",
+	"crank_in TIMESTAMPTZ NOT NULL",
+	"crank_up TIMESTAMPTZ NOT NULL",
+	"start_date TIMESTAMPTZ NOT NULL",
+	"release_date TIMESTAMPTZ NOT NULL",
+	"vfx_due_date TIMESTAMPTZ NOT NULL",
 	"output_size STRING NOT NULL",
 	"view_lut STRING NOT NULL",
 }
 
-// ordMapFromProject는 프로젝트 정보를 OrdMap에 담는다.
-func ordMapFromProject(p *Project) *ordMap {
-	o := newOrdMap()
-	o.Set("id", p.ID)
-	o.Set("name", p.Name)
-	o.Set("status", p.Status)
-	o.Set("client", p.Client)
-	o.Set("director", p.Director)
-	o.Set("producer", p.Producer)
-	o.Set("vfx_supervisor", p.VFXSupervisor)
-	o.Set("vfx_manager", p.VFXManager)
-	o.Set("cg_supervisor", p.CGSupervisor)
-	o.Set("crank_in", p.CrankIn)
-	o.Set("crank_up", p.CrankUp)
-	o.Set("start_date", p.StartDate)
-	o.Set("release_date", p.ReleaseDate)
-	o.Set("vfx_due_date", p.VFXDueDate)
-	o.Set("output_size", p.OutputSize)
-	o.Set("view_lut", p.ViewLUT)
-	return o
+// AddProject는 db에 프로젝트를 추가한다.
+func AddProject(db *sql.DB, p *Project) error {
+	if p == nil {
+		return errors.New("nil Project is invalid")
+	}
+	keystr := strings.Join(ProjectTableKeys, ", ")
+	idxstr := strings.Join(ProjectTableIndices, ", ")
+	stmt := fmt.Sprintf("INSERT INTO projects (%s) VALUES (%s)", keystr, idxstr)
+	if _, err := db.Exec(stmt, p.dbValues()...); err != nil {
+		return err
+	}
+	// TODO: add project info, task, tracking table
+	if err := CreateTableIfNotExists(db, p.ID+"_shots", ShotTableFields); err != nil {
+		return err
+	}
+	return nil
+}
+
+// ProjectExist는 db에 해당 프로젝트가 존재하는지를 검사한다.
+func ProjectExist(db *sql.DB, prj string) (bool, error) {
+	rows, err := db.Query("SELECT id FROM projects WHERE id=$1 LIMIT 1", prj)
+	if err != nil {
+		return false, err
+	}
+	return rows.Next(), nil
+}
+
+// projectFromRows는 테이블의 한 열에서 프로젝트를 받아온다.
+func projectFromRows(rows *sql.Rows) (*Project, error) {
+	p := &Project{}
+	err := rows.Scan(
+		&p.ID, &p.Name, &p.Status, &p.Client,
+		&p.Director, &p.Producer, &p.VFXSupervisor, &p.VFXManager, &p.CGSupervisor,
+		&p.CrankIn, &p.CrankUp, &p.StartDate, &p.ReleaseDate, &p.VFXDueDate, &p.OutputSize,
+		&p.ViewLUT,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return p, nil
+}
+
+// GetProject는 db에서 특정 프로젝트 정보를 부른다.
+// 해당 프로젝트가 없다면 nil이 반환된다.
+func GetProject(db *sql.DB, prj string) (*Project, error) {
+	keystr := strings.Join(ProjectTableKeys, ", ")
+	stmt := fmt.Sprintf("SELECT %s FROM projects WHERE id=$1", keystr)
+	rows, err := db.Query(stmt, prj)
+	if err != nil {
+		return nil, err
+	}
+	if !rows.Next() {
+		return nil, nil
+	}
+	p, err := projectFromRows(rows)
+	if err != nil {
+		return nil, err
+	}
+	return p, nil
+}
+
+// SearchAllProjects는 db에서 모든 프로젝트 정보를 가져온다.
+// 검색 중 문제가 있으면 nil, error를 반환한다.
+func SearchAllProjects(db *sql.DB) ([]*Project, error) {
+	fields := strings.Join(ProjectTableKeys, ", ")
+	stmt := fmt.Sprintf("SELECT %s FROM projects", fields)
+	rows, err := db.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+	prjs := make([]*Project, 0)
+	for rows.Next() {
+		p, err := projectFromRows(rows)
+		if err != nil {
+			return nil, err
+		}
+		prjs = append(prjs, p)
+	}
+	if rows.Err() != nil {
+		return nil, rows.Err()
+	}
+	return prjs, nil
 }
--- project_test.go
+++ project_test.go
@@ -1,13 +1,44 @@
 package roi
 
 import (
+	"database/sql"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
 	"reflect"
 	"testing"
 	"time"
 )
 
-func TestOrdMapFromProject(t *testing.T) {
-	p := &Project{
+func TestMain(m *testing.M) {
+	tempDir, err := ioutil.TempDir("", "cockroach-test-")
+	if err != nil {
+		log.Fatal(err)
+	}
+	cmd := exec.Command(
+		"cockroach", "start", "--insecure",
+		"--http-addr=localhost:5454", "--listen-addr=localhost:54545",
+		fmt.Sprintf("--store=path=%s", tempDir),
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	time.Sleep(time.Second)
+
+	code := m.Run()
+
+	cmd.Process.Kill()
+	os.RemoveAll(tempDir)
+	os.Exit(code)
+}
+
+func TestProject(t *testing.T) {
+	want := &Project{
 		ID:            "TEST",
 		Name:          "테스트 프로젝트",
 		Status:        "waiting",
@@ -17,34 +48,34 @@ func TestOrdMapFromProject(t *testing.T) {
 		VFXSupervisor: "김성환",
 		VFXManager:    "조경식",
 		CGSupervisor:  "김용빈",
-		CrankIn:       time.Date(2018, 12, 31, 7, 30, 0, 0, time.Local),
-		CrankUp:       time.Date(2019, 8, 31, 19, 0, 0, 0, time.Local),
-		StartDate:     time.Date(2018, 12, 29, 0, 0, 0, 0, time.Local),
-		ReleaseDate:   time.Date(2018, 10, 1, 0, 0, 0, 0, time.Local),
-		VFXDueDate:    time.Date(2018, 9, 31, 0, 0, 0, 0, time.Local),
+		CrankIn:       time.Date(2018, 12, 31, 7, 30, 0, 0, time.Local).UTC(),
+		CrankUp:       time.Date(2019, 8, 31, 19, 0, 0, 0, time.Local).UTC(),
+		StartDate:     time.Date(2018, 12, 29, 0, 0, 0, 0, time.Local).UTC(),
+		ReleaseDate:   time.Date(2018, 10, 1, 0, 0, 0, 0, time.Local).UTC(),
+		VFXDueDate:    time.Date(2018, 9, 31, 0, 0, 0, 0, time.Local).UTC(),
 		OutputSize:    "1920x1080",
 		ViewLUT:       "some/place/aces.lut",
 	}
-	got := ordMapFromProject(p)
 
-	want := newOrdMap()
-	want.Set("id", "TEST")
-	want.Set("name", "테스트 프로젝트")
-	want.Set("status", "waiting")
-	want.Set("client", "레이지 픽처스")
-	want.Set("director", "윤지은")
-	want.Set("producer", "김한웅")
-	want.Set("vfx_supervisor", "김성환")
-	want.Set("vfx_manager", "조경식")
-	want.Set("cg_supervisor", "김용빈")
-	want.Set("crank_in", time.Date(2018, 12, 31, 7, 30, 0, 0, time.Local))
-	want.Set("crank_up", time.Date(2019, 8, 31, 19, 0, 0, 0, time.Local))
-	want.Set("start_date", time.Date(2018, 12, 29, 0, 0, 0, 0, time.Local))
-	want.Set("release_date", time.Date(2018, 10, 1, 0, 0, 0, 0, time.Local))
-	want.Set("vfx_due_date", time.Date(2018, 9, 31, 0, 0, 0, 0, time.Local))
-	want.Set("output_size", "1920x1080")
-	want.Set("view_lut", "some/place/aces.lut")
-
+	db, err := sql.Open("postgres", "postgresql://root@localhost:54545/roi?sslmode=disable")
+	if err != nil {
+		t.Fatalf("error connecting to the database: %s", err)
+	}
+	if _, err := db.Exec("CREATE DATABASE IF NOT EXISTS roi"); err != nil {
+		log.Fatal("error creating db 'roi': ", err)
+	}
+	err = CreateTableIfNotExists(db, "projects", ProjectTableFields)
+	if err != nil {
+		t.Fatalf("could not create projects table: %s", err)
+	}
+	err = AddProject(db, want)
+	if err != nil {
+		t.Fatalf("could not add project to projects table: %s", err)
+	}
+	got, err := GetProject(db, want.ID)
+	if err != nil {
+		t.Fatalf("could not get project to projects table: %s", err)
+	}
 	if !reflect.DeepEqual(got, want) {
 		t.Fatalf("got: %v, want: %v", got, want)
 	}
