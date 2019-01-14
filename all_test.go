0c5d77b8421ccf61ec480257f0e7a4589cff7b31
kim yongbin <kybinz@gmail.com>
2019-01-14 22:10:36 +0900

    테스트가 죽더라도 cockroach db 인스턴스와 디렉토리 지움 (#119)

--- all_test.go
+++ all_test.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"os/signal"
+	"syscall"
 	"testing"
 	"time"
 )
@@ -27,6 +29,15 @@ func TestMain(m *testing.M) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	// 테스트가 죽었을 때 cockroach DB 및 임시 디렉토리 삭제
+	s := make(chan os.Signal, 1)
+	signal.Notify(s, syscall.SIGTERM, syscall.SIGINT, syscall.SIGKILL)
+	go func() {
+		<-s
+		cmd.Process.Kill()
+		os.RemoveAll(tempDir)
+		os.Exit(1)
+	}()
 	// DB가 시작되기까지 시간 필요
 	time.Sleep(time.Second)
 
