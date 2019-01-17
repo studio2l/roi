35379454b4793514b568447bab8f3e60812969ed
kim yongbin <kybinz@gmail.com>
2019-01-17 16:39:00 +0900

    User에서 ordMap 삭제 및 실제 DB를 이용한 테스트로 변경 (#125)
    Update: #109

--- db.go
+++ db.go
@@ -2,12 +2,9 @@ package roi
 
 import (
 	"database/sql"
-	"errors"
 	"fmt"
 	_ "image/jpeg"
 	"strings"
-
-	"golang.org/x/crypto/bcrypt"
 )
 
 // CreateTableIfNotExists는 db에 해당 테이블이 없을 때 추가한다.
@@ -47,112 +44,4 @@ func pgIndices(n int) []string {
 		idxs[i] = fmt.Sprintf("$%d", i+1)
 	}
 	return idxs
-}
-
-// AddUser는 db에 한 명의 사용자를 추가한다.
-func AddUser(db *sql.DB, id, pw string) error {
-	// 이 이름을 가진 사용자가 이미 있는지 검사한다.
-	rows, err := SelectAll(db, "users", map[string]string{"id": id})
-	if err != nil {
-		return err
-	}
-	if rows.Next() {
-		return fmt.Errorf("user %s already exists", id)
-	}
-	// 패스워드 해시
-	hashed, err := bcrypt.GenerateFromPassword([]byte(pw), bcrypt.DefaultCost)
-	if err != nil {
-		return err
-	}
-	hashed_password := string(hashed)
-	// 사용자 생성
-	m := NewUserMap(id, hashed_password)
-	keystr := strings.Join(m.Keys(), ", ")
-	idxstr := strings.Join(pgIndices(m.Len()), ", ")
-	stmt := fmt.Sprintf("INSERT INTO users (%s) VALUES (%s)", keystr, idxstr)
-	fmt.Println(stmt)
-	if _, err := db.Exec(stmt, m.Values()...); err != nil {
-		return err
-	}
-	return nil
-}
-
-// GetUser는 db에서 사용자를 검색한다.
-// 해당 유저를 찾지 못하면 nil이 반환된다.
-func GetUser(db *sql.DB, id string) (*User, error) {
-	stmt := "SELECT id, kor_name, name, team, position, email, phone_number, entry_date FROM users WHERE id=$1"
-	fmt.Println(stmt)
-	rows, err := db.Query(stmt, id)
-	if err != nil {
-		return nil, err
-	}
-	ok := rows.Next()
-	if !ok {
-		return nil, nil
-	}
-	u := &User{}
-	if err := rows.Scan(&u.ID, &u.KorName, &u.Name, &u.Team, &u.Position, &u.Email, &u.PhoneNumber, &u.EntryDate); err != nil {
-		return nil, err
-	}
-	return u, nil
-}
-
-// UserHasPassword는 db에 저장된 사용자의 비밀번호와 입력된 비밀번호가 같은지를 비교한다.
-// 해당 사용자가 없거나, 불러오는데 에러가 나면 false와 에러를 반환한다.
-func UserHasPassword(db *sql.DB, id, pw string) (bool, error) {
-	stmt := "SELECT hashed_password FROM users WHERE id=$1"
-	fmt.Println(stmt)
-	rows, err := db.Query(stmt, id)
-	if err != nil {
-		return false, err
-	}
-	ok := rows.Next()
-	if !ok {
-		return false, fmt.Errorf("user '%s' not exists", id)
-	}
-	var hashed_password string
-	rows.Scan(&hashed_password)
-	err = bcrypt.CompareHashAndPassword([]byte(hashed_password), []byte(pw))
-	if err != nil {
-		return false, err
-	}
-	return true, nil
-}
-
-// UpdateUser는 db에 비밀번호를 제외한 사용자 필드를 업데이트 한다.
-func UpdateUser(db *sql.DB, id string, u *User) error {
-	if u == nil {
-		return errors.New("nil User is invalid")
-	}
-	m := ordMapFromUser(u)
-	setstr := ""
-	i := 0
-	for _, k := range m.Keys() {
-		if i != 0 {
-			setstr += ", "
-		}
-		setstr += fmt.Sprintf("%s=$%d", k, i+1)
-		i++
-	}
-	stmt := fmt.Sprintf("UPDATE users SET %s WHERE id='%s'", setstr, id)
-	fmt.Println(stmt)
-	if _, err := db.Exec(stmt, m.Values()...); err != nil {
-		return err
-	}
-	return nil
-}
-
-// UpdateUserPassword는 db에 저장된 사용자 패스워드를 수정한다.
-func UpdateUserPassword(db *sql.DB, id, pw string) error {
-	hashed, err := bcrypt.GenerateFromPassword([]byte(pw), bcrypt.DefaultCost)
-	if err != nil {
-		return err
-	}
-	hashed_password := string(hashed)
-	stmt := "UPDATE users SET hashed_password=$1 WHERE id=$2"
-	fmt.Println(stmt)
-	if _, err := db.Exec(stmt, hashed_password, id); err != nil {
-		return err
-	}
-	return nil
 }
--- user.go
+++ user.go
@@ -1,5 +1,14 @@
 package roi
 
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
 // User는 사용자와 관련된 정보이다.
 type User struct {
 	ID          string
@@ -11,6 +20,52 @@ type User struct {
 	PhoneNumber string
 	EntryDate   string
 }
+
+func (u *User) dbValues() []interface{} {
+	if u == nil {
+		u = &User{}
+	}
+	vals := []interface{}{
+		u.ID,
+		u.KorName,
+		u.Name,
+		u.Team,
+		u.Position,
+		u.Email,
+		u.PhoneNumber,
+		u.EntryDate,
+	}
+	return vals
+}
+
+func (u *User) dbValuesWithHashedPassword(hashed_password string) []interface{} {
+	return append(u.dbValues(), hashed_password)
+}
+
+var UserTableKeys = []string{
+	"id",
+	"kor_name",
+	"name",
+	"team",
+	"position",
+	"email",
+	"phone_number",
+	"entry_date",
+}
+
+var UserTableKeysWithHashedPassword = append(
+	UserTableKeys,
+	"hashed_password",
+)
+
+var UserTableIndices = []string{
+	"$1", "$2", "$3", "$4", "$5", "$6", "$7", "$8",
+}
+
+var UserTableIndicesWithHashedPassword = append(
+	UserTableIndices,
+	"$9",
+)
 
 var UserTableFields = []string{
 	"uniqid UUID PRIMARY KEY DEFAULT gen_random_uuid()",
@@ -26,31 +81,121 @@ var UserTableFields = []string{
 	"hashed_password STRING NOT NULL",
 }
 
-// NewUserMap은 새로운 사용자를 생성할 때 쓰는 맵이다.
-func NewUserMap(id, hashed_password string) *ordMap {
-	o := newOrdMap()
-	o.Set("id", id)
-	o.Set("kor_name", "")
-	o.Set("name", "")
-	o.Set("team", "")
-	o.Set("position", "")
-	o.Set("email", "")
-	o.Set("phone_number", "")
-	o.Set("entry_date", "")
-	o.Set("hashed_password", hashed_password)
-	return o
+// AddUser는 db에 한 명의 사용자를 추가한다.
+func AddUser(db *sql.DB, id, pw string) error {
+	// 이 이름을 가진 사용자가 이미 있는지 검사한다.
+	exist, err := UserExist(db, id)
+	if err != nil {
+		return err
+	}
+	if exist {
+		return fmt.Errorf("user already exists: %s", id)
+	}
+	// 패스워드 해시
+	hashed, err := bcrypt.GenerateFromPassword([]byte(pw), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	hashed_password := string(hashed)
+	// 사용자 생성
+	keystr := strings.Join(UserTableKeysWithHashedPassword, ", ")
+	idxstr := strings.Join(UserTableIndicesWithHashedPassword, ", ")
+	stmt := fmt.Sprintf("INSERT INTO users (%s) VALUES (%s)", keystr, idxstr)
+	u := &User{ID: id}
+	if _, err := db.Exec(stmt, u.dbValuesWithHashedPassword(hashed_password)...); err != nil {
+		return err
+	}
+	return nil
 }
 
-// ordMapFromUser는 유저 정보를 OrdMap에 담는다.
-func ordMapFromUser(u *User) *ordMap {
-	o := newOrdMap()
-	o.Set("id", u.ID)
-	o.Set("kor_name", u.KorName)
-	o.Set("name", u.Name)
-	o.Set("team", u.Team)
-	o.Set("position", u.Position)
-	o.Set("email", u.Email)
-	o.Set("phone_number", u.PhoneNumber)
-	o.Set("entry_date", u.EntryDate)
-	return o
+// UserExist는 db에서 사용자가 존재하는지 검사한다.
+func UserExist(db *sql.DB, id string) (bool, error) {
+	stmt := fmt.Sprintf("SELECT id FROM users WHERE id='%s'", id)
+	rows, err := db.Query(stmt)
+	if err != nil {
+		return false, err
+	}
+	return rows.Next(), rows.Err()
+}
+
+// GetUser는 db에서 사용자를 검색한다.
+// 해당 유저를 찾지 못하면 nil이 반환된다.
+func GetUser(db *sql.DB, id string) (*User, error) {
+	keystr := strings.Join(UserTableKeys, ", ")
+	stmt := fmt.Sprintf("SELECT %s FROM users WHERE id='%s'", keystr, id)
+	rows, err := db.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+	ok := rows.Next()
+	if !ok {
+		return nil, nil
+	}
+	u := &User{}
+	if err := rows.Scan(&u.ID, &u.KorName, &u.Name, &u.Team, &u.Position, &u.Email, &u.PhoneNumber, &u.EntryDate); err != nil {
+		return nil, err
+	}
+	return u, nil
+}
+
+// UserHasPassword는 db에 저장된 사용자의 비밀번호와 입력된 비밀번호가 같은지를 비교한다.
+// 해당 사용자가 없거나, 불러오는데 에러가 나면 false와 에러를 반환한다.
+func UserHasPassword(db *sql.DB, id, pw string) (bool, error) {
+	stmt := fmt.Sprintf("SELECT hashed_password FROM users WHERE id='%s'", id)
+	rows, err := db.Query(stmt)
+	if err != nil {
+		return false, err
+	}
+	ok := rows.Next()
+	if !ok {
+		return false, fmt.Errorf("user '%s' not exists", id)
+	}
+	var hashed_password string
+	rows.Scan(&hashed_password)
+	err = bcrypt.CompareHashAndPassword([]byte(hashed_password), []byte(pw))
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+// UpdateUser는 db에 비밀번호를 제외한 사용자 필드를 업데이트 한다.
+func UpdateUser(db *sql.DB, id string, u *User) error {
+	if id == "" {
+		return errors.New("empty id")
+	}
+	if u == nil {
+		return errors.New("nil User is invalid")
+	}
+	keystr := strings.Join(UserTableKeys, ", ")
+	idxstr := strings.Join(UserTableIndices, ", ")
+	stmt := fmt.Sprintf("UPDATE users SET (%s) = (%s) WHERE id='%s'", keystr, idxstr, id)
+	if _, err := db.Exec(stmt, u.dbValues()...); err != nil {
+		return err
+	}
+	return nil
+}
+
+// UpdateUserPassword는 db에 저장된 사용자 패스워드를 수정한다.
+func UpdateUserPassword(db *sql.DB, id, pw string) error {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(pw), bcrypt.DefaultCost)
+	if err != nil {
+		return fmt.Errorf("could not generate hash from password: %v", err)
+	}
+	hashed_password := string(hashed)
+	stmt := fmt.Sprintf("UPDATE users SET hashed_password=$1 WHERE id='%s'", id)
+	if _, err := db.Exec(stmt, hashed_password); err != nil {
+		return err
+	}
+	return nil
+}
+
+// DeleteUser는 해당 id의 사용자를 지운다.
+// 만일 해당 아이디의 사용자가 없다면 에러를 낸다.
+func DeleteUser(db *sql.DB, id string) error {
+	stmt := fmt.Sprintf("DELETE FROM users WHERE id='%s'", id)
+	if _, err := db.Exec(stmt); err != nil {
+		return err
+	}
+	return nil
 }
