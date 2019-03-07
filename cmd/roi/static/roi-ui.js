a1d14b771a310f56c6afe7dc2cdacd5612202c33
Cho Kyungsik <33473665+kaycho@users.noreply.github.com>
2019-03-07 16:19:05 +0900

    Set project type (#257)
    * project 설정 페이지에 project type 선택 드롭메뉴 추가
    
    * 인덴테이션 수정

--- cmd/roi/static/roi-ui.js
+++ cmd/roi/static/roi-ui.js
@@ -1,5 +1,17 @@
 // 상단 메뉴의 등록 및 설정 메뉴 드롭다운 활성화
-$('#add-menu').dropdown();
-$('#user-menu').dropdown();
+$('#add-menu')
+    .dropdown()
+;
+$('#user-menu')
+    .dropdown()
+;
+
 // 언어 선택 radio
-$('.ui.language.radio.checkbox').checkbox();
+$('.ui.language.radio.checkbox')
+    .checkbox()
+;
+
+// 상단 메뉴의 등록 및 설정 메뉴 드롭다운 활성화
+$('#project-type')
+    .dropdown()
+;
