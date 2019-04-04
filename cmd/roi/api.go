37fbcea0e72c7c7e552384b94dbf2cb4eb910dfe
kim yongbin <kybinz@gmail.com>
2019-04-04 13:11:03 +0900

    apiResponse 타입 만들고 api 요청에 대한 응답 함수 추가 (#273)

--- cmd/roi/api.go
+++ cmd/roi/api.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,70 +12,82 @@ import (
 	"github.com/studio2l/roi"
 )
 
-// addShotApiHander는 사용자가 api를 통해 샷을 생성할수 있도록 한다.
-// 응답은 json 형식이고, 샷이 잘 생성되었다면 .msg,
-// 샷 생성에 문제가 있었다면 .err에 메시지가 담긴다.
-func addShotApiHandler(w http.ResponseWriter, r *http.Request) {
-	type response struct {
-		Msg string `json:"msg"`
-		Err string `json:"err"`
-	}
+// apiResponse는 /api/ 하위 사이트로 사용자가 질의했을 때 json 응답을 위해 사용한다.
+type apiResponse struct {
+	Msg string `json:"msg"`
+	Err string `json:"err"`
+}
 
+// apiOK는 api 질의가 잘 처리되었을 때
+// 그 응답을 apiResponse.Msg에 담아 반환한다.
+func apiOK(w http.ResponseWriter, msg string) {
+	w.WriteHeader(http.StatusOK)
+	resp, _ := json.Marshal(apiResponse{Msg: msg})
+	w.Write(resp)
+}
+
+// apiInternalServerError는 내부적인 이유로 api 질의의 처리에 실패했을 때
+// 이를 질의자에게 알린다. (이유는 알리지 않는다.)
+func apiInternalServerError(w http.ResponseWriter) {
+	resp, _ := json.Marshal(apiResponse{Err: "internal error"})
+	http.Error(w, string(resp), http.StatusInternalServerError)
+}
+
+// apiBadRequest는 api 질의에 문제가 있었을 때
+// 그 문제를 apiReponse.Err에 담아 반환한다.
+func apiBadRequest(w http.ResponseWriter, err error) {
+	if err == nil {
+		// err 가 nil이어서는 안되지만, 패닉을 일으키는 것보다는 낫다.
+		err = errors.New("error not explained")
+	}
+	resp, _ := json.Marshal(apiResponse{Err: err.Error()})
+	http.Error(w, string(resp), http.StatusInternalServerError)
+}
+
+// addShotApiHander는 사용자가 api를 통해 샷을 생성할수 있도록 한다.
+// 결과는 apiResponse의 json 형식으로 반환된다.
+func addShotApiHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	db, err := roi.DB()
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		resp, _ := json.Marshal(response{Err: fmt.Sprintf("internal error")})
-		w.Write(resp)
+		log.Printf("could not connect to db: %v", err)
+		apiInternalServerError(w)
 		return
 	}
 
 	prj := r.PostFormValue("project")
 	if prj == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		resp, _ := json.Marshal(response{Err: fmt.Sprintf("'project' not specified")})
-		w.Write(resp)
+		apiBadRequest(w, fmt.Errorf("'project' not specified"))
 		return
 	}
 	exist, err := roi.ProjectExist(db, prj)
 	if err != nil {
-		log.Print("project selection error: ", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		resp, _ := json.Marshal(response{Err: fmt.Sprintf("internal error during project selection, sorry!")})
-		w.Write(resp)
+		log.Printf("could not check project %q exist: %v", prj, err)
+		apiInternalServerError(w)
 		return
 	}
 	if !exist {
-		w.WriteHeader(http.StatusBadRequest)
-		resp, _ := json.Marshal(response{Err: fmt.Sprintf("project '%s' not exists", prj)})
-		w.Write(resp)
+		apiBadRequest(w, fmt.Errorf("project '%s' not exists", prj))
 		return
 	}
 
 	id := r.PostFormValue("id")
 	if id == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		resp, _ := json.Marshal(response{Err: fmt.Sprintf("'id' not specified")})
-		w.Write(resp)
+		apiBadRequest(w, fmt.Errorf("shot 'id' not specified"))
 		return
 	}
 	if !roi.IsValidShotID(id) {
-		w.WriteHeader(http.StatusBadRequest)
-		resp, _ := json.Marshal(response{Err: fmt.Sprintf("shot id is not valid: %s", id)})
-		w.Write(resp)
+		apiBadRequest(w, fmt.Errorf("shot id '%s' is not valid", id))
 		return
 	}
 	exist, err = roi.ShotExist(db, prj, id)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		resp, _ := json.Marshal(response{Err: fmt.Sprintf("internal error during shot check, sorry!")})
-		w.Write(resp)
+		log.Printf("could not check shot '%s' exist: %v", id, err)
+		apiInternalServerError(w)
 		return
 	}
 	if exist {
-		w.WriteHeader(http.StatusBadRequest)
-		resp, _ := json.Marshal(response{Err: fmt.Sprintf("shot '%s' already exists", id)})
-		w.Write(resp)
+		apiBadRequest(w, fmt.Errorf("shot '%s' already exists", id))
 		return
 	}
 
@@ -88,9 +101,7 @@ func addShotApiHandler(w http.ResponseWriter, r *http.Request) {
 	if v != "" {
 		e, err := strconv.Atoi(v)
 		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			resp, _ := json.Marshal(response{Err: fmt.Sprintf("could not convert edit_order to int: %s", r.PostFormValue("edit_order"))})
-			w.Write(resp)
+			apiBadRequest(w, fmt.Errorf("could not convert edit_order to int: %s", v))
 			return
 		}
 		editOrder = e
@@ -100,9 +111,7 @@ func addShotApiHandler(w http.ResponseWriter, r *http.Request) {
 	if v != "" {
 		d, err := strconv.Atoi(v)
 		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			resp, _ := json.Marshal(response{Err: fmt.Sprintf("could not convert duration to int: %s", r.PostFormValue("duration"))})
-			w.Write(resp)
+			apiBadRequest(w, fmt.Errorf("could not convert duration to int: %s", v))
 			return
 		}
 		duration = d
@@ -120,12 +129,10 @@ func addShotApiHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	err = roi.AddShot(db, prj, s)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		resp, _ := json.Marshal(response{Err: fmt.Sprintf("%s", err)})
-		w.Write(resp)
+		log.Printf("could not add shot: %v", err)
+		apiInternalServerError(w)
 		return
 	}
-	w.WriteHeader(http.StatusOK)
-	resp, _ := json.Marshal(response{Msg: fmt.Sprintf("successfully add a shot: '%s'", id)})
-	w.Write(resp)
+
+	apiOK(w, fmt.Sprintf("successfully add a shot: '%s'", id))
 }
