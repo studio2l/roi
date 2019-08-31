a8bfaae9d96b98462cac8dcaf46335b08c17306e
kim yongbin <kybinz@gmail.com>
2019-08-31 21:53:00 +0900

    cmd/roishot: db에 직접 접근하지 않고 api를 이용하여 샷을 추가 (#275)
    Close: #13

--- cmd/roi/api.go
+++ cmd/roi/api.go
@@ -12,24 +12,18 @@ import (
 	"github.com/studio2l/roi"
 )
 
-// apiResponse는 /api/ 하위 사이트로 사용자가 질의했을 때 json 응답을 위해 사용한다.
-type apiResponse struct {
-	Msg string `json:"msg"`
-	Err string `json:"err"`
-}
-
 // apiOK는 api 질의가 잘 처리되었을 때
-// 그 응답을 apiResponse.Msg에 담아 반환한다.
+// 그 응답을 roi.APIResponse.Msg에 담아 반환한다.
 func apiOK(w http.ResponseWriter, msg string) {
 	w.WriteHeader(http.StatusOK)
-	resp, _ := json.Marshal(apiResponse{Msg: msg})
+	resp, _ := json.Marshal(roi.APIResponse{Msg: msg})
 	w.Write(resp)
 }
 
 // apiInternalServerError는 내부적인 이유로 api 질의의 처리에 실패했을 때
 // 이를 질의자에게 알린다. (이유는 알리지 않는다.)
 func apiInternalServerError(w http.ResponseWriter) {
-	resp, _ := json.Marshal(apiResponse{Err: "internal error"})
+	resp, _ := json.Marshal(roi.APIResponse{Err: "internal error"})
 	http.Error(w, string(resp), http.StatusInternalServerError)
 }
 
@@ -40,12 +34,51 @@ func apiBadRequest(w http.ResponseWriter, err error) {
 		// err 가 nil이어서는 안되지만, 패닉을 일으키는 것보다는 낫다.
 		err = errors.New("error not explained")
 	}
-	resp, _ := json.Marshal(apiResponse{Err: err.Error()})
+	resp, _ := json.Marshal(roi.APIResponse{Err: err.Error()})
 	http.Error(w, string(resp), http.StatusInternalServerError)
 }
 
+// addProjectApiHander는 사용자가 api를 통해 프로젝트를 생성할수 있도록 한다.
+// 결과는 roi.APIResponse의 json 형식으로 반환된다.
+func addProjectApiHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	db, err := roi.DB()
+	if err != nil {
+		log.Printf("could not connect to db: %v", err)
+		apiInternalServerError(w)
+		return
+	}
+
+	prj := r.PostFormValue("id")
+	if prj == "" {
+		apiBadRequest(w, fmt.Errorf("'id' not specified"))
+		return
+	}
+	exist, err := roi.ProjectExist(db, prj)
+	if err != nil {
+		log.Printf("could not check project %q exist: %v", prj, err)
+		apiInternalServerError(w)
+		return
+	}
+	if exist {
+		apiBadRequest(w, fmt.Errorf("project '%s' already exists", prj))
+		return
+	}
+	p := &roi.Project{
+		ID: prj,
+	}
+	err = roi.AddProject(db, p)
+	if err != nil {
+		log.Printf("could not add project: %v", err)
+		apiInternalServerError(w)
+		return
+	}
+
+	apiOK(w, fmt.Sprintf("successfully add a project: '%s'", prj))
+}
+
 // addShotApiHander는 사용자가 api를 통해 샷을 생성할수 있도록 한다.
-// 결과는 apiResponse의 json 형식으로 반환된다.
+// 결과는 roi.APIResponse의 json 형식으로 반환된다.
 func addShotApiHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	db, err := roi.DB()
@@ -55,9 +88,9 @@ func addShotApiHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	prj := r.PostFormValue("project")
+	prj := r.PostFormValue("project_id")
 	if prj == "" {
-		apiBadRequest(w, fmt.Errorf("'project' not specified"))
+		apiBadRequest(w, fmt.Errorf("'project_id' not specified"))
 		return
 	}
 	exist, err := roi.ProjectExist(db, prj)
@@ -99,24 +132,30 @@ func addShotApiHandler(w http.ResponseWriter, r *http.Request) {
 	editOrder := 0
 	v = r.PostFormValue("edit_order")
 	if v != "" {
-		e, err := strconv.Atoi(v)
-		if err != nil {
-			apiBadRequest(w, fmt.Errorf("could not convert edit_order to int: %s", v))
-			return
-		}
-		editOrder = e
-	}
-	duration := 0
-	v = r.PostFormValue("duration")
-	if v != "" {
-		d, err := strconv.Atoi(v)
+		// strconv.Itoa 대신 ParseFloat을 쓰는 이유는 엑셀에서 분명히 정수형으로
+		// 입력한 데이터를 excelize 패키지를 통해 받을 때 실수형으로 변경되기 때문이다.
+		// 무엇이 문제인지는 확실치 않지만, 실수형으로 받은후 정수로 변환하는 것이
+		// 안전하다.
+		f, err := strconv.ParseFloat(v, 32)
 		if err != nil {
 			apiBadRequest(w, fmt.Errorf("could not convert duration to int: %s", v))
 			return
 		}
-		duration = d
+		editOrder = int(f)
+	}
+	duration := 0
+	v = r.PostFormValue("duration")
+	if v != "" {
+		// 위 내용 참조
+		f, err := strconv.ParseFloat(v, 32)
+		if err != nil {
+			apiBadRequest(w, fmt.Errorf("could not convert duration to int: %s", v))
+			return
+		}
+		duration = int(f)
 	}
 	s := &roi.Shot{
+		ProjectID:     prj,
 		ID:            id,
 		Status:        roi.ShotStatus(status),
 		EditOrder:     editOrder,
