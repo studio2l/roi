c7bdcf67254d8cf37bc2b67f370f727b33196219
kim yongbin <kybinz@gmail.com>
2019-09-20 16:46:14 +0900

    cmd/roi: html 에서 날짜를 선택할 때 html의 기본 date 인풋을 사용
    한웅씨가 date가 있다는 것을 알려줌. 고마워요.
    
    Close: #292

--- cmd/roi/template.go
+++ cmd/roi/template.go
@@ -63,12 +63,12 @@ func isSunday(t time.Time) bool {
 	return wd == time.Sunday
 }
 
-// stringFromTime은 시간을 rfc3339 형식의 문자열로 표현한다.
+// stringFromTime은 시간을 rfc3339 형식에서 존 정보를 제외한 문자열을 반환한다.
 func stringFromTime(t time.Time) string {
 	if t.IsZero() {
 		return ""
 	}
-	return t.Local().Format(time.RFC3339)
+	return t.Local().Format("2006-01-02T15:04:05")
 }
 
 // stringFromDate는 시간을 rfc3339 형식의 문자열로 표현하되 '연-월-일' 만 표시한다.
@@ -76,7 +76,7 @@ func stringFromDate(t time.Time) string {
 	if t.IsZero() {
 		return ""
 	}
-	return strings.Split(t.Local().Format(time.RFC3339), "T")[0]
+	return t.Local().Format("2006-01-02")
 }
 
 // shortStringFromDate는 시간을 rfc3339 형식의 문자열로 표현하되 '월-일' 만 표시한다.
@@ -84,13 +84,13 @@ func shortStringFromDate(t time.Time) string {
 	if t.IsZero() {
 		return ""
 	}
-	return strings.SplitN(strings.Split(t.Local().Format(time.RFC3339), "T")[0], "-", 2)[1]
+	return t.Local().Format("01-02")
 }
 
-// timeFromString는 rfc3339 형식의 문자열에서 시간을 얻는다.
+// timeFromString는 존 정보를 제외한 rfc3339 형식의 문자열에서 시간을 얻는다.
 // 받은 문자열이 형식에 맞지 않으면 에러를 반환한다.
 func timeFromString(s string) (time.Time, error) {
-	return time.Parse(time.RFC3339, s)
+	return time.ParseInLocation("2006-01-02T15:04:05", s, time.Local)
 }
 
 // parseTimeforms는 http.Request.Form에서 시간 형식의 Form에 대해 파싱해
