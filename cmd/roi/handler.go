aff213f1fa55dd7ebd47c5629b287c15e3cda907
kim yongbin <kybinz@gmail.com>
2019-11-30 00:06:02 +0900

    composite literal uses unkeyed fields 에러 고침

--- cmd/roi/handler.go
+++ cmd/roi/handler.go
@@ -14,7 +14,7 @@ import (
 func mustFields(r *http.Request, keys ...string) error {
 	for _, k := range keys {
 		if r.FormValue(k) == "" {
-			return roi.BadRequest{fmt.Sprintf("form field not found: %s", k)}
+			return roi.BadRequest{Msg: fmt.Sprintf("form field not found: %s", k)}
 		}
 	}
 	return nil
@@ -26,7 +26,7 @@ func mustFields(r *http.Request, keys ...string) error {
 func sessionUser(r *http.Request) (*roi.User, error) {
 	session, err := getSession(r)
 	if err != nil {
-		return nil, roi.Internal{errors.New("could not get session")}
+		return nil, roi.Internal{Err: errors.New("could not get session")}
 	}
 	user := session["userid"]
 	if user == "" {
@@ -46,19 +46,19 @@ func saveFormFile(r *http.Request, field string, dst string) error {
 	}
 	defer f.Close()
 	if fi.Size > (32 << 20) {
-		return roi.BadRequest{fmt.Sprintf("mov: file size too big (got %dMB, maximum 32MB)", fi.Size>>20)}
+		return roi.BadRequest{Msg: fmt.Sprintf("mov: file size too big (got %dMB, maximum 32MB)", fi.Size>>20)}
 	}
 	data, err := ioutil.ReadAll(f)
 	if err != nil {
-		return roi.Internal{fmt.Errorf("could not read file data: %w", err)}
+		return roi.Internal{Err: fmt.Errorf("could not read file data: %w", err)}
 	}
 	err = os.MkdirAll(filepath.Dir(dst), 0755)
 	if err != nil {
-		return roi.Internal{fmt.Errorf("could not create directory: %w", err)}
+		return roi.Internal{Err: fmt.Errorf("could not create directory: %w", err)}
 	}
 	err = ioutil.WriteFile(dst, data, 0755)
 	if err != nil {
-		return roi.Internal{fmt.Errorf("could not save file: %w", err)}
+		return roi.Internal{Err: fmt.Errorf("could not save file: %w", err)}
 	}
 	return nil
 }
