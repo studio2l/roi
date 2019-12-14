f90abfd6874f21e0795a6e8617c6f894486fcd3c
kim yongbin <kybinz@gmail.com>
2019-12-14 17:25:45 +0900

    유저 인풋 자동완성 리스트 구현 (#390)
    대부분은 CSI의 코드를 베끼긴 했지만 나름대로 정리하고
    기능도 조금 수정하였다.
    
    Semantic-UI와 충돌이 있어 위치가 잘못 나오는데 이는 추후
    수정할 예정이다. 아직 태스크 업데이트 페이지에만 적용되어 있다.

--- cmd/roi/static/roi-ui.js
+++ cmd/roi/static/roi-ui.js
@@ -1,17 +1,9 @@
 // 상단 메뉴의 등록 및 설정 메뉴 드롭다운 활성화
-$('#add-menu')
-    .dropdown()
-;
-$('#user-menu')
-    .dropdown()
-;
+$('#add-menu').dropdown()
+$('#user-menu').dropdown()
 
 // 언어 선택 radio
-$('.ui.language.radio.checkbox')
-    .checkbox()
-;
+$('.ui.language.radio.checkbox').checkbox()
 
 // 상단 메뉴의 등록 및 설정 메뉴 드롭다운 활성화
-$('#show-type')
-    .dropdown()
-;
+$('#show-type').dropdown()
